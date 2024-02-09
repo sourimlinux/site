1c756dce933778697a0a4114700a1bdddc06595b
Cat <99858531+PatrickStar8753@users.noreply.github.com>
2024-02-09 11:17:49 +0700

    Added site

---
+++ js/adaptation.js
@@ -0,0 +1,20 @@
+var useragent = navigator.userAgent.toLowerCase();
+if (useragent.indexOf("mobile") > -1 || useragent.indexOf("android") > -1 || window.innerWidth <= 925){
+    document.querySelector('body').classList.add('mobile');
+    document.getElementById('logobutton').removeAttribute("href");
+}
+
+window.onresize = function() {
+    var location = window.location.href.toString().split('/');
+    if (window.innerWidth <= 925){
+        document.querySelector('body').classList.add('mobile');
+        document.getElementById('logobutton').removeAttribute("href");
+    } else {
+        document.querySelector('body').classList.remove('mobile');
+        if (location[location.length - 2] == "news"){
+            document.getElementById('logobutton').setAttribute("href", "../");
+        } else {
+            document.getElementById('logobutton').setAttribute("href", "/");
+        }   
+    }
+}
